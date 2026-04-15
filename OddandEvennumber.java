158cb7240893d283812fb31c0cd868c4248e8516
siam <bdshiam330@gmail.com>
2026-04-15 09:36:46 +0600

    Create OddandEvennumber.java

---
+++ OddandEvennumber.java
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter your frist number:");
+    scanf("%d",&a);
+    printf("enter your secend number:");
+    scanf("%d",&b);
+    if(a%2==0){
+        printf("the frist number is even\n");
+
+    }
+    else{
+
+        printf("the frist number is odd\n");
+    }
+    if(b%2==0){
+        printf("the secend number is even");
+    }
+    else{
+        printf("the secend number is odd");
+    }
+    }
+
+
